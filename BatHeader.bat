90262d7130f37b7e0382ab8e4f6100b4b76ffaf3
Philip Otter <78989650+Philip-Otter@users.noreply.github.com>
2022-07-18 00:47:18 -0800

    initial commit

---
+++ BatHeader.bat
@@ -0,0 +1,84 @@
+::===============================================::
+::Name                                           ::
+::DATE                                           ::
+::                                               ::
+::Info                                           ::
+::===============================================::
+
+
+@echo off
+echo ::===============================================::
+echo ::               Batch Header Adder              ::
+echo ::                  Philip Otter                 ::
+echo ::                                               ::
+echo ::                Created July 2022              ::
+echo ::===============================================::
+echo.
+echo Please enter the file name (with extension) that you would like to add a header to:  
+echo.
+set /p Input=
+echo.
+echo Now can I also have your name?
+set /p Name=
+
+:continue
+if exist %Input% (
+    goto :doesExist
+) else (
+    goto :doesNotExist
+)
+
+:doesExist
+
+:: Deletes possible old temp files and makes a new one
+del BHAtemp.bat
+cls
+type %Input% > BHAtemp.bat
+
+:: The box spacing is set to match my name length by default.
+echo::===============================================:: >> %Input%
+echo::%Name%                                   :: >> %Input%
+echo::%date%                                 :: >> %Input%
+echo::                                               :: >> %Input%
+echo::Info                                           :: >> %Input%
+echo::===============================================:: >> %Input%
+
+echo %input% now reads:  
+echo.
+type %input%
+:: Default for choice is y or n
+choice /M "Would you like to revert your file?"
+if ERRORLEVEL ==1 goto :revert
+if ERRORLEVEL ==2 goto :keep
+
+
+
+
+:doesNotExist
+echo File does not exist within the current directory!
+timeout /t 15
+exit
+
+
+:cleanup
+del BHAtemp.bat
+exit
+
+:revert
+type BHAtemp.bat > %Input%
+del BHAtemp.bat
+echo your file has been reverted and now reads:
+type %Input%
+pause
+exit
+
+
+:keep
+del BHAtemp.bat
+echo Goodbye!
+pause
+exit
+
+:delOldTemp
+del BHAtemp.bat
+goto :continue
---
+++ README.txt
@@ -0,0 +1,2 @@
+These are just a collection of batch scripts that I have made to make my life easier. Feel free to use them or modify them however you may see fit.
+You are using these at your own risk please do your research and look at the code before you run something.
